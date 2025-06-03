91a3e0b0b24a4442321b9fe006ce2bda851cc6f6
Supriya9876 <137330585+Supriya9876@users.noreply.github.com>
2025-06-03 12:17:00 +0530

    Update kube.groovy

--- vars/kube.groovy
+++ vars/kube.groovy
@@ -1,7 +1,9 @@
-def call() {
+def call(String kubeconfigPath) {
+    withEnv(["KUBECONFIG=${kubeconfigPath}"]) {
         sh '''
             kubectl apply -f dep.yaml
             kubectl apply -f service.yaml
         '''
+    }
 }
 
